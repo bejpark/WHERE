6a82364948ce3df12253316e922aab4c6e432dc2
BeJ Park <bjpark0506@naver.com>
2020-10-04 18:50:07 +0900

    1차수정

---
+++ app/src/main/java/com/cau/where/MainActivity.java
@@ -0,0 +1,17 @@
+package com.cau.where;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
